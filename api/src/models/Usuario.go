45e011ff04802b69ee22b0c13182637de5ad0014
wallacemachado <wallacefm18@gmail.com>
2021-08-16 18:33:57 -0300

    add model de usuario

---
+++ api/src/models/Usuario.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+//Usuario representa um usuário utilizando a rede social
+type Usuario struct {
+	ID uint64 `json:"id,omitempty"` // omitempty para que se o id for vazio não enviar para o bd
+
+	Nome     string    `json:"nome,omitempty"`
+	Nick     string    `json:"nick,omitempty"`
+	Email    string    `json:"email,omitempty"`
+	Senha    string    `json:"senha,omitempty"`
+	CriadoEm time.Time `json:"CriadoEm,omitempty"`
+}
