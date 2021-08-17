89f62d70ad8088edf76cd5f6a3c8005a2cba3762
wallacemachado <wallacefm18@gmail.com>
2021-08-17 08:04:04 -0300

    respostas no padrão JSON

--- api/src/respostas/respostas.go
+++ api/src/respostas/respostas.go
@@ -8,7 +8,7 @@ import (
 
 // JSON retorna uma resposta em JSON para a requisição
 func JSON(w http.ResponseWriter, statusCode int, dados interface{}) {
-
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
 	if dados != nil {
