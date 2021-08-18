64598d06d2d5533479c34f628aded18cad2b5f3c
wallacemachado <wallacefm18@gmail.com>
2021-08-18 14:29:16 -0300

    GERANDO SENHA SECRETA COM BASE 64

--- api/main.go
+++ api/main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"api/src/config"
 	"api/src/router"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+func init() {
+	chave := make([]byte, 64)
+
+	if _, erro := rand.Read(chave); erro != nil {
+		log.Fatal(erro)
+	}
+
+	stringbase64 := base64.StdEncoding.EncodeToString(chave)
+
+	fmt.Println(stringbase64)
+}
 
 func main() {
 	config.Carregar()
