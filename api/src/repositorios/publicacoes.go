5304762f3a9b459a7cc17994ebc5ede032cd4394
wallacemachado <wallacefm18@gmail.com>
2021-08-19 09:10:21 -0300

    atualizar publicações

--- api/src/repositorios/publicacoes.go
+++ api/src/repositorios/publicacoes.go
@@ -107,3 +107,18 @@ func (repositorio Publicacoes) Buscar(usuarioID uint64) ([]models.Publicacao, er
 
 	return publicacoes, nil
 }
+
+// Atualizar altera os dados de uma publicação no banco de dados
+func (repositorio Publicacoes) Atualizar(publicacaoID uint64, publicacao models.Publicacao) error {
+	statement, erro := repositorio.db.Prepare("update publicacoes set titulo = ?, conteudo = ? where id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(publicacao.Titulo, publicacao.Conteudo, publicacaoID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
