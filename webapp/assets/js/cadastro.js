0d553a5f40e9df5171a16f33c97e7f9e29930d04
wallacemachado <wallacefm18@gmail.com>
2021-08-20 12:19:41 -0300

    ajustes

--- webapp/assets/js/cadastro.js
+++ webapp/assets/js/cadastro.js
@@ -2,6 +2,7 @@ $('#formulario-cadastro').on('submit', criarUsuario);
 
 function criarUsuario(evento) {
     evento.preventDefault();
+    console.log("cheguei")
 
     if ($('#senha').val() != $('#confirmar-senha').val()) {
         alert( "As senhas não coincidem!");
@@ -17,23 +18,5 @@ function criarUsuario(evento) {
            nick: $('#nick').val(),
            senha: $('#senha').val()
         }
-    }).done(function() {
-        Swal.fire("Sucesso!", "Usuário cadastrado com sucesso!", "success")
-            .then(function() {
-                $.ajax({
-                    url: "/login",
-                    method: "POST",
-                    data: {
-                        email: $('#email').val(),
-                        senha: $('#senha').val()
-                    }
-                }).done(function() {
-                    window.location = "/home";
-                }).fail(function() {
-                    Swal.fire("Ops...", "Erro ao autenticar o usuário!", "error");
-                })
-            })
-    }).fail(function() {
-        Swal.fire("Ops...", "Erro ao cadastrar o usuário!", "error");
     });
 }
