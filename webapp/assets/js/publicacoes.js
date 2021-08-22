e207cb22f967b906455d32f03cf67d02ce081543
wallacemachado <wallacefm18@gmail.com>
2021-08-22 16:03:44 -0300

    curtir publicação

--- webapp/assets/js/publicacoes.js
+++ webapp/assets/js/publicacoes.js
@@ -1,5 +1,5 @@
 $('#nova-publicacao').on('submit', criarPublicacao);
-
+$(document).on('click', '.curtir-publicacao', curtirPublicacao);
 
 
 function criarPublicacao(evento) {
@@ -19,6 +19,32 @@ function criarPublicacao(evento) {
     }).fail(function() {
         alert("Erro ao criar a publicação!");
     })
+}
+
+function curtirPublicacao(evento) {
+    evento.preventDefault();
+
+    const elementoClicado = $(evento.target);
+    const publicacaoId = elementoClicado.closest('div').data('publicacao-id');
+
+    //loading até que a reqquisição seja concluída
+    elementoClicado.prop('disabled', true);
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}/curtir`,
+        method: "POST"
+    }).done(function() {
+        const contadorDeCurtidas = elementoClicado.next('span');
+        const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+        contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
+
+       
+    }).fail(function() {
+        alert("Erro ao curtir a publicação!");
+    }).always(function() {
+        elementoClicado.prop('disabled', false);
+        //apos a requisição
+    });
 }
 
 
