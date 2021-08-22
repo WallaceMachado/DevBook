6be96ac8c52211a2353a1c7cb9047cb297438b67
wallacemachado <wallacefm18@gmail.com>
2021-08-22 14:17:07 -0300

    criação de publicações

---
+++ webapp/assets/js/publicacoes.js
@@ -0,0 +1,24 @@
+$('#nova-publicacao').on('submit', criarPublicacao);
+
+
+
+function criarPublicacao(evento) {
+    evento.preventDefault();
+
+    console.log("publicacoes")
+
+    $.ajax({
+        url: "/publicacoes",
+        method: "POST",
+        data: {
+            titulo: $('#titulo').val(),
+            conteudo: $('#conteudo').val(),
+        }
+    }).done(function() {
+        window.location = "/home";
+    }).fail(function() {
+        alert("Erro ao criar a publicação!");
+    })
+}
+
+
