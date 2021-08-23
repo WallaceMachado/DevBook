f5bbbe582ace1645e0e01f14e485599157fb6722
wallacemachado <wallacefm18@gmail.com>
2021-08-22 21:40:54 -0300

    pagina perfil do usuário buscado

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -146,8 +146,6 @@ func CarregarPerfilDoUsuario(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Println(usuario)
-
 	utils.ExecutarTemplate(w, "usuario.html", struct {
 		Usuario         modelos.Usuario
 		UsuarioLogadoID uint64
